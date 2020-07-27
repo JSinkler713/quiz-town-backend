60ff93de0bc1c1fa6f5c590f23e192e0633bcf7b
James Sinkler <jamessinkler713@gmail.com>
2020-07-26 19:06:33 -0700

    changes question Schema to have requirement of a Quiz relationship

--- models/question.js
+++ models/question.js
@@ -2,11 +2,16 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema;
 
 const QuestionSchema = new Schema({
-  prompt: { type: String, required: true, unique: true }
+  prompt: { type: String, required: true, unique: true },
   answers: [{
       text: String,
       correct: Boolean
     }],
+  quiz: {
+    type: Schema.Types.ObjectId,
+    ref: 'Quiz',
+    required: true,
+  }
 })
 
 const Question = mongoose.model('Question', QuestionSchema);
--- models/quiz.js
+++ models/quiz.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema;
 
 const QuizSchema = new Schema({
-  title: { type: String, required: true, unique: true }
+  title: { type: String, required: true, unique: true },
   questions: [{
       type: Schema.Types.ObjectId,
       ref: 'Question',
