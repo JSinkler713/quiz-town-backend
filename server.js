94fb154b77e73f37340b30587838e0631c009c96
James Sinkler <jamessinkler713@gmail.com>
2020-07-26 21:29:10 -0700

    sets up a create and show route for quiz

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const routes = require('./routes')
-
+const db = require('./models');
 // Middleware - JSON parsing
 app.use(express.json())
 
@@ -15,7 +15,26 @@ const corsOptions = {
 app.use(cors(corsOptions))
 */
 // API routes
-app.use('/api/v1/quiz', routes.quiz)
+//app.use('/api/v1/quiz', routes.quizzes)
+
+
+const show = (req,res) => {
+  db.Quiz.find( {name: req.params.name }, (err, foundQuiz)=> {
+    res.status(200).json({ quiz: foundQuiz });
+  })
+}
+
+const create = (req, res) => {
+  db.Quiz.create(req.body, (err, createdQuiz) => {
+    if (err) console.log('error', err);
+    res.status(200).json( { quiz: createdQuiz } )
+  });
+}
+
+// quiz routes
+app.post('/api/v1/quiz', create);
+app.get('/api/v1/quiz/:name', show);
+
 
 // Listening
 app.listen(4000, ()=> console.log('server running on port 4000'))
