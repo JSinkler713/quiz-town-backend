2b0054c8e53cd7d321ba442e61002e506c432f37
James Sinkler <jamessinkler713@gmail.com>
2020-09-06 17:43:30 -0700

    creates route for search/:name for quizzes

--- routes/quizzes.js
+++ routes/quizzes.js
@@ -4,8 +4,9 @@ const ctrl = require('../controllers')
 
 //routes
 router.post('/', ctrl.quizzes.create);
-router.get('/', ctrl.quizzes.index);
+router.get('/search/:name', ctrl.quizzes.searchQuizzes);
 router.get('/:name', ctrl.quizzes.show);
+router.get('/', ctrl.quizzes.index);
 
 
 module.exports = router
